b63dd7fee704b8179fbfd1f98c3d314899d2d97e
Ryan Grimes <ryanpaulgrimes@gmail.com>
2021-12-31 13:05:29 +0100

    format navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,10 +6,11 @@ export default function Navbar() {
     <div className="navbar">
       <div className="wrapper">
         <div className="left">
-          <h2><FaceOutlined className='icon'/> Ryan Grimes</h2>
+          <h1 className="name"><FaceOutlined className='icon'/> Ryan Grimes</h1>
           <h3><a href="#intro" className="logo">Intro</a></h3>
+          <h3><a href="#portfolio" className="logo">Porfolio</a></h3>
+          <h3><a href="#contact" className="logo">Contact</a></h3>
         <div className="itemContainer">
-          
           <span>123456</span>
         </div>
         </div>
