ca8c1b6e1b08e459d3219b5af1047b004c0b9d92
Ryan Grimes <ryanpaulgrimes@gmail.com>
2021-12-31 12:23:47 +0100

    Added sections, page snapping

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,19 @@
 import "./navbar.scss"
+import {FaceOutlined} from "@material-ui/icons"
 
 export default function Navbar() {
   return (
     <div className="navbar">
       <div className="wrapper">
-        
+        <div className="left">
+          <h2><FaceOutlined className='icon'/> Ryan Grimes</h2>
+          <h3><a href="#intro" className="logo">Intro</a></h3>
+        <div className="itemContainer">
+          
+          <span>123456</span>
+        </div>
+        </div>
+        <div className='right'></div>
       </div>
     </div>
   )
