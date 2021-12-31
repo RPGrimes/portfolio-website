ab576d5d248a1a406ffd192e952c08e96c137f87
Ryan Grimes <ryanpaulgrimes@gmail.com>
2021-12-31 15:55:25 +0100

    corrected error where link to CV in menu wasn't working properly

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -11,7 +11,7 @@ export default function Menu({ menuOpen, setMenuOpen }) {
           <a href="#portfolio">Portfolio</a>
         </li>
         <li onClick={()=>setMenuOpen(false)}>
-          <a href="#contact">CV</a>
+          <a href="#CV">CV</a>
         </li>
         <li onClick={()=>setMenuOpen(false)}>
           <a href="#contact">Contact Details</a>
