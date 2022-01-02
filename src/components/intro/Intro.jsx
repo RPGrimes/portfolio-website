907e5dba72f9643e5a309c4495dec5dd7c791f6e
Ryan Grimes <ryanpaulgrimes@gmail.com>
2022-01-02 14:51:06 +0100

    add language and framework images to intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -4,17 +4,37 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export default function Intro() {
   return (
-    <div className="intro">
-      <div className='github'>
-        <h1><GitHubIcon className="icon"/><a href="https://github.com/RPGrimes">My Github</a></h1>
+    <div className="intro" id="intro">
+      <div className="skills">
+        <div className="imgContainer">
+          <div className="logo" id="javascript">
+            <img src="assets/jslogo.png" alt="JavaScript"/>
+            <img src="assets/reactlogo.png" alt="React"/>
+            <img src="assets/nodejslog.png" alt="Node"/>
+            <img src="assets/jest-logo.png" alt="Jest"/>
+            <h5>Javascript</h5>
+          </div>
+          <div className="logo" id="ruby">
+            <img src="assets/ruby-logo.png" alt="Ruby"/>
+            <img src="assets/railslogo.png" alt="Rails"/>
+            <img src="assets/sinatra.png" alt="Sinatra"/>
+            <img src="assets/rspec-logo.png" alt="RSpec"/>
+            <h5>Ruby</h5>
+          </div>
+          <div className="logo" id="other">
+            <img src="assets/gitlogo.png" alt="Git"/>
+            <img src="assets/htmllogo.png" alt="HTML"/>
+            <img src="assets/csslogo.png" alt="CSS"/>
+            <img src="assets/sasslogo.png" alt="Sass"/>
+            <h5>Other</h5>
+          </div>
+        </div>
       </div>
-      <div className="linkedin">
+
+      <div className='profiles'>
+        <h1><GitHubIcon className="icon"/><a href="https://github.com/RPGrimes">My Github</a></h1>
         <h1><LinkedInIcon className="icon"/><a href="https://www.linkedin.com/in/ryanpgrimes/">My LinkedIn</a></h1>
       </div>
     </div>
   )
 }
-
-
-
-
--- src/components/setmenu/setMenu.jsx
+++ src/components/setmenu/setMenu.jsx
@@ -1,9 +1,10 @@
-// export default function setMenu({props, setMenuOpen}) {
-//   return (
-//     <div className="setMenu" id="setMenu">
-//       <li onClick={()=>setMenuOpen(false)}>
-//         <a href={`#${props.page}`}>text</a>
-//       </li>;
-//     </div>
-//   )
-// }
+export default function setMenu({props, setMenuOpen}) {
+  return (
+    <div className="setMenu" id="setMenu">
+      <li onClick={()=>setMenuOpen(false)}>
+        <a href={`#${props.page}`}>text</a>
+      </li>;
+    </div>
+  )
+}
+
