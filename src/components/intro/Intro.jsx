e6c2236a0b5aad9030ef7b3d705238946831aa71
Ryan Grimes <ryanpaulgrimes@gmail.com>
2022-01-02 15:59:36 +0100

    About me added

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -6,34 +6,72 @@ export default function Intro() {
   return (
     <div className="intro" id="intro">
       <div className="skills">
+
+        <div className="bio" id="hello">
+          <h1>Hello, my name is Ryan! I am a full stack web developer, currently based in Gibraltar.</h1>
+        </div>
+
+        <div className="bio" id="about">
+          <p>I am a full stack web developer looking to join a team in a positive and supportive environment where principles such as TDD are followed and where a growth mindset is encouraged.</p>
+          <p>I am full committed to the philosophy of life-long learning.</p>
+          <p>The unique blend of creativity, logic, technology and an almost unlimited scope to develop and learn drives my fascination and passion for web development.</p>
+          <p>When I'm not at my computer I enjoy spending my time playing sports, indulging in new cuisines and enjoying my PlayStation!</p>
+        </div>
+
         <div className="imgContainer">
           <div className="logo" id="javascript">
-            <img src="assets/jslogo.png" alt="JavaScript"/>
-            <img src="assets/reactlogo.png" alt="React"/>
-            <img src="assets/nodejslog.png" alt="Node"/>
-            <img src="assets/jest-logo.png" alt="Jest"/>
-            <h5>Javascript</h5>
+            <a href="https://www.javascript.com/">
+              <img src="assets/jslogo.png" alt="JavaScript"/>
+            </a> 
+            <a href="https://reactjs.org/">
+              <img src="assets/reactlogo.png" alt="React"/>
+            </a>
+            <a href="https://nodejs.org/en/">
+              <img src="assets/nodejslog.png" alt="Node"/>
+            </a> 
+            <a href="https://jestjs.io/">
+              <img src="assets/jest-logo.png" alt="Jest"/> 
+            </a> 
+            <p>Javascript</p>
           </div>
+        
           <div className="logo" id="ruby">
-            <img src="assets/ruby-logo.png" alt="Ruby"/>
-            <img src="assets/railslogo.png" alt="Rails"/>
-            <img src="assets/sinatra.png" alt="Sinatra"/>
-            <img src="assets/rspec-logo.png" alt="RSpec"/>
-            <h5>Ruby</h5>
+            <a href="https://www.ruby-lang.org/en/">
+              <img src="assets/ruby-logo.png" alt="Ruby"/>
+            </a>
+            <a href="https://rubyonrails.org/">
+              <img src="assets/railslogo.png" alt="Rails"/>
+            </a>
+            <a href="http://sinatrarb.com/">
+              <img src="assets/sinatra.png" alt="Sinatra"/>
+            </a>
+            <a href="https://rspec.info/">
+              <img src="assets/rspec-logo.png" alt="RSpec"/>
+            </a>
+            <p>Ruby</p>
           </div>
+
           <div className="logo" id="other">
-            <img src="assets/gitlogo.png" alt="Git"/>
-            <img src="assets/htmllogo.png" alt="HTML"/>
-            <img src="assets/csslogo.png" alt="CSS"/>
-            <img src="assets/sasslogo.png" alt="Sass"/>
-            <h5>Other</h5>
+            <a href="https://git-scm.com/">
+              <img src="assets/gitlogo.png" alt="Git"/>
+            </a>
+            <a href="https://html.spec.whatwg.org/multipage/">
+              <img src="assets/htmllogo.png" alt="HTML"/>
+            </a>
+            <a href="https://www.w3.org/Style/CSS/">
+              <img src="assets/csslogo.png" alt="CSS"/>
+            </a>
+            <a href="https://sass-lang.com/">
+              <img src="assets/sasslogo.png" alt="Sass"/>
+            </a>
+            <p>Other</p>
           </div>
         </div>
       </div>
 
       <div className='profiles'>
-        <h1><GitHubIcon className="icon"/><a href="https://github.com/RPGrimes">My Github</a></h1>
-        <h1><LinkedInIcon className="icon"/><a href="https://www.linkedin.com/in/ryanpgrimes/">My LinkedIn</a></h1>
+        <p><GitHubIcon className="icon"/><a href="https://github.com/RPGrimes">My Github</a></p>
+        <p><LinkedInIcon className="icon"/><a href="https://www.linkedin.com/in/ryanpgrimes/">My LinkedIn</a></p>
       </div>
     </div>
   )
