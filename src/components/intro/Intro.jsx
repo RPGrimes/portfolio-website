ae21c7260cebd01d04ee242a7f0cbbb20556aa91
Ryan Grimes <ryanpaulgrimes@gmail.com>
2022-01-08 11:55:04 +0100

    Add intro page text animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -2,32 +2,46 @@ import "./intro.scss"
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import SchoolIcon from '@mui/icons-material/School';
+import { init } from 'ityped';
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+
+  const textRef = useRef();
+
+  useEffect(()=>{
+    init(textRef.current, {
+      showCursor: false,
+      typeSpeed: 100,
+      strings: ["Welcome to my website!", "I am a full stack web developer."],
+    });
+  },[]);
+
   return (
     <div className="intro" id="intro">
       <div className="skills">
 
         <div className="bio" id="hello">
-          <h2>Hello, my name is</h2><h1> Ryan!</h1><h3>I'm a full stack web developer.</h3>
+          <h2>Hello, my name is</h2><h1> Ryan</h1>
+          <h3> .<span ref={textRef}></span></h3>
         </div>
 
       </div>
 
       <div className='profiles'>
-        <div className="profile">
+        <div className="profile" id="github">
         <a href="https://github.com/RPGrimes">
           <GitHubIcon className="icon"/>
           <p>My Github</p></a>
         </div>
 
-        <div className="profile">
+        <div className="profile" id="CV">
         <a href="https://docs.google.com/presentation/d/1-uBieG_R5u-NvwhT7xLpx46s4v4jCQUqwHug0It2erc/edit#slide=id.p">
           <SchoolIcon className="icon"/>
           <p>My CV</p></a>
         </div>
 
-        <div className="profile">
+        <div className="profile" id="linkedin">
         <a href="https://www.linkedin.com/in/ryanpgrimes/">
           <LinkedInIcon className="icon" />
           <p>My LinkedIn</p></a>
