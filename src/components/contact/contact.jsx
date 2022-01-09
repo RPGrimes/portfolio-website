74630b1b98ea9ff9ce49590fb00a73ef140ddd08
Ryan Grimes <ryanpaulgrimes@gmail.com>
2022-01-09 18:10:08 +0100

    Contact layout done, need to set up to send email address next

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,10 +1,20 @@
 import "./contact.scss"
 
 export default function Contact() {
+
   return (
     <div className="contact" id="contact">
       <div className="title">
-        <p>Contact Details</p>
+        <p>Contact Me!</p>
+      </div>
+      <div className="emailForm">
+        {/* <form onSubmit={handleSubmit}> */}
+        <form>
+          <input type='text' placeholder='Email'/>
+          <textarea placeholder="Message"></textarea>
+          <button type='submit'>Send</button>
+          {/* {message && <span>Thank you for your message! I will reply as soon as possible.</span>} */}
+        </form>
       </div>
     </div>
   )
