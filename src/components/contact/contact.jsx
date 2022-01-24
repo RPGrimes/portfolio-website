7e6becac46fed98e5766da0e67b055110577f39d
Ryan Grimes <ryanpaulgrimes@gmail.com>
2022-01-24 13:27:34 +0100

    Email form logs info to console

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -3,16 +3,19 @@ import { useForm } from 'react-hook-form';
 
 export default function Contact() {
 
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = data => console.log(data);
+
   return (
     <div className="contact" id="contact">
       <div className="title">
         <p>Contact Me!</p>
       </div>
       <div className="emailForm">
-        <form>
-          <input type='email' name="userEmail" placeholder='Email'/>
-          <textarea placeholder="Message"></textarea>
-          <button type='submit'>Send</button>
+        <form id='contact-form' onSubmit={handleSubmit(onSubmit)}>
+          <input type='email' name="userEmail" {...register("email")} placeholder='Email'/>
+          <textarea name='message' {...register("message")} placeholder="Message"></textarea>
+          <input type='submit' value='Send' />
         </form>
       </div>
     </div>
