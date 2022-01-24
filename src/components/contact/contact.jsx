d7c2e5e61179d91c8e4b914d0bf4692cd031f03d
Ryan Grimes <ryanpaulgrimes@gmail.com>
2022-01-24 13:48:40 +0100

    basic form validation added

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -12,10 +12,10 @@ export default function Contact() {
         <p>Contact Me!</p>
       </div>
       <div className="emailForm">
-        <form id='contact-form' onSubmit={handleSubmit(onSubmit)}>
-          <input type='email' name="userEmail" {...register("email")} placeholder='Email'/>
-          <textarea name='message' {...register("message")} placeholder="Message"></textarea>
-          <input type='submit' value='Send' />
+      <form id='contact-form' onSubmit={handleSubmit(onSubmit)}>
+        <input type='email' name="userEmail" {...register("email", {required: true})} placeholder='Email' maxLength='80' required/>
+        <textarea name='message' {...register("message")} placeholder="Message" maxLength='1000' required></textarea>
+          <button type='submit'>Send</button>
         </form>
       </div>
     </div>
